5cdc5afbb431c5b12d4c31e7c1cdfed2135ffd3a
Artem <artkruzhkov@gmail.com>
2023-06-18 20:20:53 +0300

    hw03final

--- HW03/test.java
+++
@@ -1,9 +0,0 @@
-package HW03;
-
-public class test {
-    public static void main(String[] args) {
-        String test = "1777";
-        int i = Integer.parseInt(test);
-        System.out.println(i);
-    }
-}
